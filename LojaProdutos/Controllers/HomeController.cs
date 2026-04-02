cfdf700a2ed035fa5739a9576a5977a32d04af0f
Daniel-Ferreira-2004 <danikferreira69@gmail.com>
2026-04-02 17:41:48 -0300

    inicial

---
+++ LojaProdutos/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaProdutos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
