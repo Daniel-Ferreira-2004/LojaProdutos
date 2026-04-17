53d24191397f7e1b25d15fba31f644b2b6b4ba5e
Daniel-Ferreira-2004 <danikferreira69@gmail.com>
2026-04-17 15:44:46 -0300

    Add DataTables CDN and table id
    Include DataTables CSS/JS from CDN in _Layout.cshtml and add id="registros" to the products table in Views/Produtos/Index.cshtml so the product list can be initialized with DataTables. Project build/VS metadata files (bin/obj/.vs) were also updated/regenerated as part of the build/IDE state changes.

--- LojaProdutos/wwwroot/js/site.js
+++ LojaProdutos/wwwroot/js/site.js
@@ -1,9 +1,37 @@
 ﻿$(document).ready(function () {
 
-    setTimeout(function () {
+    $('#registros').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
 
-    $('.alert').fadeOut("slow", function () {
-        $(this).alert("close");
+        "language": { // 👈 versão mais atual usa "language"
+            "emptyTable": "Nenhum registro encontrado na tabela",
+            "info": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "infoFiltered": "(Filtrar de _MAX_ total registros)",
+            "lengthMenu": "Mostrar _MENU_ registros por página",
+            "loadingRecords": "Carregando...",
+            "processing": "Processando...",
+            "zeroRecords": "Nenhum registro encontrado",
+            "search": "Pesquisar",
+            "paginate": {
+                "next": "Próximo",
+                "previous": "Anterior",
+                "first": "Primeiro",
+                "last": "Último"
+            },
+            "aria": {
+                "sortAscending": ": Ordenar colunas de forma ascendente",
+                "sortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
     });
-}, 4000);
-})
+
+    setTimeout(function () {
+        $('.alert').fadeOut("slow", function () {
+            $(this).alert("close");
+        });
+    }, 4000);
+
+});
